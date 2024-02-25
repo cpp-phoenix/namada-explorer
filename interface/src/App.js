ccdf71577063a531def17f5df86339dea64d4e06
cpp-phoenix <gurjinders92571@gmail.com>
2024-02-26 01:04:38 +0530

    complete page 1

---
+++ interface/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Blocks from './pages/Blocks';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="flex flex-col bg-black w-screen h-screen fixed">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/blocks' exact element={<Blocks/>}/>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/search' exact element={<Search/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
